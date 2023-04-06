0f61a66a5de00a4c5d0bdd2f47b88f99c9427956
truboder <danil@rock-and-roll.ru>
2023-04-06 10:00:03 +0300

    change

---
+++ Assets/Scripts/SpawnScriptableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpawnObject", menuName = "ScriptableObjects/SpawnObject", order = 1)]
+
+public class SpawnScriptableObject : ScriptableObject
+{
+    [SerializeField] public GameObject Prefab;  
+}
---
+++ Assets/Scripts/UnitsSpawning.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitsSpawning : MonoBehaviour
+{
+    [SerializeField] private Transform _parentPoint;
+    [SerializeField] private SpawnScriptableObject _unit;
+    [SerializeField] private float _delay = 2f;
+    [SerializeField] private GameObject[] _spawnPoints;
+
+    private void Start()
+    {
+        StartCoroutine("spawnUnits");
+    }
+
+    private IEnumerator spawnUnits()
+    {
+        yield return new WaitForSeconds(_delay);
+
+        foreach (var spawnPoint in _spawnPoints)
+        {
+            Instantiate(_unit.Prefab, spawnPoint.transform);
+            yield return new WaitForSeconds(_delay);
+        }
+    }
+}
